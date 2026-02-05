b2c5810265f6f50b38b6f049942f0086867aa3bf
DeltaUniqx <uniqxdeslandes@gmail.com>
2026-02-05 17:23:38 -0500

    More Glitch Update

--- Assets/Antoine Folder/Script/GlitchCube.cs
+++ Assets/Antoine Folder/Script/GlitchCube.cs
@@ -5,9 +5,32 @@ public class GlitchCube : MonoBehaviour
     [SerializeField] Vector2 RandomSizeSmallCube;
     [SerializeField] Vector2 RandomSizeBigCube;
 
+    [SerializeField] Vector2 ChangeSize;
+    float timerSize;
+
+    [SerializeField] Vector2 ShakeRandom;
+    float shake;
+    Vector3 ogPosition;
+
     private void Start()
     {
         transform.localScale = RandomSize();
+        ogPosition = transform.position;
+        shake = Random.Range(ShakeRandom.x, ShakeRandom.y);
+
+        timerSize = Random.Range(ChangeSize.x, ChangeSize.y);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = ogPosition + RandomPose();
+
+        timerSize -= Time.deltaTime;
+        if (timerSize <= 0)
+        {
+            transform.localScale = RandomSize();
+            timerSize = Random.Range(ChangeSize.x, ChangeSize.y);
+        }
     }
 
     Vector3 RandomSize()
@@ -26,6 +49,17 @@ public class GlitchCube : MonoBehaviour
             y = Random.Range(RandomSizeBigCube.x, RandomSizeBigCube.y);
             z = Random.Range(RandomSizeBigCube.x, RandomSizeBigCube.y);
         }
+
+        return new Vector3(x, y, z);
+    }
+
+    Vector3 RandomPose()
+    {
+        float x; float y; float z;
+
+        x = Random.Range(-shake, shake);
+        y = Random.Range(-shake, shake);
+        z = Random.Range(-shake, shake);
 
         return new Vector3(x, y, z);
     }
